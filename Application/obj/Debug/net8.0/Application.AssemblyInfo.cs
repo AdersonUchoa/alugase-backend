3b16d9e5dd03acb327262fa24fa18ca31b27f5f4
Aderson <iamuchoa@gmail.com>
2025-12-20 23:50:58 -0300

    bin

--- Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec1b77e24d9581e4e49336550f629c2c09c57e80")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd5fce4c20e02fdc4ad3bbe5016e04a724bb574b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
