b391ac71209962fd44aa23aa4a80dfa13c7acf02
Aderson <iamuchoa@gmail.com>
2025-12-24 16:15:08 -0300

    fix swagger

--- AlugaSe.WebAPI/Program.cs
+++ AlugaSe.WebAPI/Program.cs
@@ -13,11 +13,12 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "AlugaSe API v1");
+    c.RoutePrefix = "swagger";
+});
 
 app.UseHttpsRedirection();
 
